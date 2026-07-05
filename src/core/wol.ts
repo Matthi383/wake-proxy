0d9c100a713295af030d9af37a5797a4e189b8a2
Burner <github@radl.systems>
2026-07-05 08:48:43 +0200

    updated example in devices.yaml

--- src/core/wol.ts
+++ src/core/wol.ts
@@ -4,7 +4,9 @@ import wol from "wake_on_lan";
 export const wakeDevice = (mac: string): Promise<void> => {
   return new Promise((resolve, reject) => {
     wol.wake(mac, (err: any) => {
-      if (err) return reject(err);
+      if (err) {
+        return reject(err);
+      }
       resolve();
     });
   });
